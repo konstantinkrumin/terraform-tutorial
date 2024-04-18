5504821c82adcad7ebccbd632cf2b404ea15b076
Konstantin Krumin <konstantin.krumin@gmail.com>
2024-04-17 22:56:12 -0400

    Add outputs

--- main.tf
+++ main.tf
@@ -103,6 +103,9 @@ resource "aws_eip" "one" {
   depends_on = [ aws_internet_gateway.gw ]
 }
 
+output "server_public_ip" {
+  value = aws_eip.one.public_ip
+}
 
 resource "aws_instance" "web-server-instance" {
   ami = "ami-352562356test"
@@ -126,4 +129,12 @@ resource "aws_instance" "web-server-instance" {
   tags = {
     Name = "web-server"
   }
+}
+
+output "server_private_ip" {
+  value = aws_instance.web-server-instance.private_ip
+}
+
+output "server_id" {
+  value = aws_instance.web-server-instance.id
 }
